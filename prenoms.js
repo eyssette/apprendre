b01f5d6e9dbe0a8501e66007598e5aa57b70bb86
Eyssette <cedric.eyssette@gmail.com>
2023-09-05 00:11:30 +0200

    Nom composés + Boutons afficher / facile / difficile

--- prenoms.js
+++ prenoms.js
@@ -1,33 +1,33 @@
 javascript:(function() {
   var images = document.getElementsByTagName('img');
-  var htmlContent = '<html><head><title>Images de la Page</title></head><style>div{display:none; margin-top:10px; text-align:center;}</style><body>';
+  var htmlContent = '<html><head><title>Images de la Page</title></head><style>div{display:none; margin-top:10px; text-align:center;} section {display:none;} button {margin:10px;}</style><body>';
   
   for (var i = 0; i < images.length; i++) {
     var image = images[i];
     var imageSrc = image.src;
     var imageAlt = image.alt;
     if (!image.alt ) {
-      var srcSansParametres = imageSrc.split("?")[0]  ;
+      var srcSansParametres = imageSrc.split("?")[0];
       var indexDerniereBarreOblique = srcSansParametres.lastIndexOf("/");
-      var nomPrenom = srcSansParametres.substring(indexDerniereBarreOblique + 1).split('_');
-      var nom = nomPrenom[0];
-      var prenom = nomPrenom[1];
-      if (prenom) {
-        prenom = decodeURIComponent(prenom.split('.')[0]);
-      }
+      var nomPrenom = decodeURIComponent(srcSansParametres.substring(indexDerniereBarreOblique + 1).replace(".jpg",""));
+	  var index = nomPrenom.search(/[a-zà-ÿ]/);
+	  var partiePrenom = nomPrenom.substring(index-1);
+	  var partieNom = nomPrenom.substring(0, index-1);
+      var nom = partieNom.replaceAll('_',' ');
+      var prenom = partiePrenom.replaceAll('_',' ');
       var imageTitle = prenom +' '+nom;
-
       if (imageSrc) {
         htmlContent += '<div>';
         htmlContent += '<img src="' + imageSrc + '" /><br>';
-        htmlContent += '<span>' + imageTitle + '</span><br>';
-        htmlContent += '<button onclick="eleveSuivant()">Élève suivant</button>';
-        htmlContent += '</div>';
+        htmlContent += '<button onclick="montrerNomPrenom()">Montrer la réponse</button>';
+        htmlContent += '<section><span>' + imageTitle + '</span><br>';
+		htmlContent += '<button onclick="difficile()">Difficile</button>';
+		htmlContent += '<button onclick="facile()">Facile</button></section></div>';
       }
     }
   }
   
-  htmlContent += '<script>var eleves = document.getElementsByTagName("div"); var index = 0;  eleves[index].style.display = "block"; function eleveSuivant() { eleves[index].style.display = "none"; index = (index + 1) % eleves.length; eleves[index].style.display = "block"; };</script>';
+  htmlContent += '<script>var eleves = document.getElementsByTagName("div"); var index = 0;  eleves[index].style.display = "block"; var sectionsReponse = document.getElementsByTagName("section"); function montrerNomPrenom(){ sectionsReponse[index].style.display="block"}; function eleveSuivant() { sectionsReponse[index].style.display="none"; eleves[index].style.display = "none"; index = (index + 1) % eleves.length; eleves[index].style.display = "block"; }; function difficile() {eleveSuivant()}; function facile() {eleveSuivant()}; </script>';
   htmlContent += '</body></html>';
   var newWindow = window.open();
   newWindow.document.open();
