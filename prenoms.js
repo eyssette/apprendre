7613e9c55eacb556f5932728de1b602521c79997
Eyssette <cedric.eyssette@gmail.com>
2023-09-05 01:34:15 +0200

    Gestion fin de la série

--- prenoms.js
+++ prenoms.js
@@ -1,6 +1,6 @@
 javascript:(function() {
   var images = document.getElementsByTagName('img');
-  var htmlContent = '<html><head><title>Images de la Page</title></head><style>div{display:none; margin-top:10px; text-align:center;} section {display:none;} button {margin:10px;}</style><body>';
+  var htmlContent = '<html><head><title>Images de la Page</title></head><style>div{margin-top:10px; text-align:center;} section, footer, div {display:none;} button {margin:10px;} footer {display: flex; justify-content: center; align-items: center height: 50%; font-size: 20px;}</style><body>';
   
   for (var i = 0; i < images.length; i++) {
     var image = images[i];
@@ -26,8 +26,8 @@ javascript:(function() {
       }
     }
   }
-  
-  htmlContent += '<script>var eleves = document.getElementsByTagName("div"); var index = 0; var show = false; var indexElevesFaciles=[]; eleves[index].style.display = "block"; var sectionsReponse = document.getElementsByTagName("section"); function montrerNomPrenom(){ sectionsReponse[index].style.display="block"; show = true}; function eleveSuivant() {show = false; do {sectionsReponse[index].style.display="none"; eleves[index].style.display = "none"; index = (index + 1) % eleves.length;} while (indexElevesFaciles.includes(index)); eleves[index].style.display = "block"; }; function difficile() {eleveSuivant()}; function facile() {indexElevesFaciles.push(index); setTimeout(function () { var positionIndex = indexElevesFaciles.indexOf(index); if (positionIndex !== -1) {indexElevesFaciles.splice(positionIndex, 1); } }, 2 * 60 * 1000); eleveSuivant()}; window.addEventListener("keydown", function (event) { if (event.keyCode === 13) {montrerNomPrenom();} if (event.keyCode === 37 && show == true) { difficile(); } if (event.keyCode === 39  && show == true) { facile(); }  });</script>';
+  htmlContent += '<footer>Bravo, vous connaissez tous les élèves de votre classe !</footer>';
+  htmlContent += '<script>var eleves = document.getElementsByTagName("div"); var index = 0; var show = false; var indexElevesFaciles=[]; eleves[index].style.display = "block"; var sectionsReponse = document.getElementsByTagName("section"); var footer = document.querySelector("footer"); function montrerNomPrenom(){ sectionsReponse[index].style.display="block"; show = true}; function eleveSuivant() {show = false; do {sectionsReponse[index].style.display="none"; eleves[index].style.display = "none"; index = (index + 1) % eleves.length;} while (indexElevesFaciles.includes(index)); eleves[index].style.display = "block"; }; function difficile() {eleveSuivant()}; function facile() {indexElevesFaciles.push(index); if (indexElevesFaciles.length == eleves.length) {eleves[index].style.display = "none"; footer.style.display = "block";} else {eleveSuivant()}}; window.addEventListener("keydown", function (event) { if (event.keyCode === 13) {montrerNomPrenom();} if (event.keyCode === 37 && show == true) { difficile(); } if (event.keyCode === 39  && show == true) { facile(); }  });</script>';
   htmlContent += '</body></html>';
   var newWindow = window.open();
   newWindow.document.open();
